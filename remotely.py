5594d20b34516b0e94b0529d39c034ba8a8de852
pulsedemon <msquashic@gmail.com>
2013-01-30 14:20:57 -0500

    version 0.2, first commit

---
+++ remotely.py
@@ -0,0 +1,16 @@
+import sublime, sublime_plugin, os
+
+def get_filepath_settings(view, string):
+  remotely_filepaths = view.settings().get(string)
+  return remotely_filepaths
+
+class Remotely(sublime_plugin.EventListener):
+  def on_post_save(self, view):
+    remotely_filepaths = get_filepath_settings(view, 'remotely_filepaths')
+
+    for dirname, target in remotely_filepaths.iteritems():
+      if view.file_name().startswith( dirname ):
+        target = target.replace( "$1", view.file_name() )
+        target = target.replace( "$2", view.file_name()[len(dirname):] )
+
+        os.system( target + " &" )
