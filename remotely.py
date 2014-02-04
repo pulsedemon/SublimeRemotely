688a6cbf306258c285bdd090a18bec897a6ae13a
Michael Squashic <msquashic@google.com>
2014-02-04 11:57:05 -0500

    added support for sublime text 3

--- remotely.py
+++ remotely.py
@@ -1,16 +1,20 @@
-import sublime, sublime_plugin, os
+import sublime
+import sublime_plugin
+import os
+
 
 def get_filepath_settings(view, string):
   remotely_filepaths = view.settings().get(string)
   return remotely_filepaths
 
+
 class Remotely(sublime_plugin.EventListener):
   def on_post_save(self, view):
     remotely_filepaths = get_filepath_settings(view, 'remotely_filepaths')
 
-    for dirname, target in remotely_filepaths.iteritems():
-      if view.file_name().startswith( dirname ):
-        target = target.replace( "$1", view.file_name() )
-        target = target.replace( "$2", view.file_name()[len(dirname):] )
+    for dirname, target in remotely_filepaths.items():
+      if view.file_name().startswith(dirname):
+        target = target.replace("$1", view.file_name())
+        target = target.replace("$2", view.file_name()[len(dirname):])
 
-        os.system( target + " &" )
+        os.system(target + " &")
